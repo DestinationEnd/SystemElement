948c7d3d0c8481e1dd0df184114e79b1e568b20c
Anton.Manzhos <antonm1.manzhos@gmail.com>
2018-07-13 17:50:04 +0300

    updated DB

--- SystemElement/Models/Element.cs
+++ SystemElement/Models/Element.cs
@@ -9,14 +9,6 @@ namespace SystemElement.Models
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public int? Sort { get; set; }
         public int? ParentId { get; set; }
-        public Element Parent { get; set; }
-
-        public ICollection<Element> Children { get; set; }
-        public Element()
-        {
-            Children = new List<Element>();
-        }
     }
 }
